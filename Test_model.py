f9d1f12c17bb85d32b13202ffbf847051e91145d
Razvan Avesalon <89531556+RazvanAVESALON@users.noreply.github.com>
2021-11-19 13:08:22 +0200

    Create Test_model.py

---
+++ Test_model.py
@@ -0,0 +1,32 @@
+
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPool2D
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dropout
+import tensorflow_datasets as tfds
+from numpy import unique
+from numpy import argmax
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from plot_acc_loss import plot_acc_loss 
+from tensorflow.keras.applications import VGG16
+import yaml
+
+config = None
+with open('config.yml') as f: # reads .yml/.yaml files
+    config = yaml.load(f)
+
+dataset_dir  = config['net']['dir']
+BATCH_SIZE = config['train']['bs']
+
+
+validation_datagen = ImageDataGenerator(rescale=1./255)    
+
+new_model = tf.keras.models.load_model('damn.h5')
+new_model.summary()
+
+test_generator = validation_datagen.flow_from_directory(dataset_dir + '/test', target_size=config['net']['img'], batch_size=config['train']['bs'], class_mode='binary')
+test_loss, test_acc = new_model.evaluate_generator(test_generator, steps=len(test_generator))
+print('test acc:', test_acc)
